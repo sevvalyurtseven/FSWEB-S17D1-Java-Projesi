25078c74351f2455989925f34ad7c2d2d6910620
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-16 01:36:43 +0300

    spring initializr araciligi ile dependencyler yüklendi spring uygulamasi kurulumu yapildi

---
+++ src/main/java/com/workintech/s17g1/S17g1Application.java
@@ -0,0 +1,13 @@
+package com.workintech.s17g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17g1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17g1Application.class, args);
+	}
+
+}
